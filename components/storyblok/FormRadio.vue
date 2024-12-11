35435173109742336ad280fce3e1867f3dd08ab5
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2024-12-11 14:23:16 -0500

    add form radio

---
+++ components/storyblok/FormRadio.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="form-group mb-6" v-editable="blok">
+        <label>{{ blok.label }}</label>
+        <input type="radio" :name="blok.name" :value="blok.value" />
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({ blok: Object });
+</script>
